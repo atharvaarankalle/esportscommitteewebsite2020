e8d8a9f569b21b2897865380e58ca6340804a59b
GodTervsy <atharvanz@gmail.com>
2020-08-25 16:57:44 +1200

    replacing php code

--- feedbackform.php
+++ feedbackform.php
@@ -1,40 +1,19 @@
-<?php 
-$errors = "";
+<?php
 
-$email = 'stu18005@ashs.school.nz';
+$name_error = "";
 
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['comments'])) {
-
-    $errors .="\n Error: All Fields are Required.";
+if(empty($_POST["name"])) {
+    $name_error = "Your name must be entered!";
+} else {
+    $user_name = $_POST["name"];
+    echo "Your name is " .$user_name. "<br>";
 }
 
-$name = $_POST['name'];
-
-$email_address = $_POST['email'];
-
-$comments = $_POST['comments'];
-
-if(!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address)) {
-
-        $errors .= "\n Error: Invalid email address";
-    }
-
-if( empty($errors)) {
-    $to = $email;
-
-    $email_subject = "Feedback from: $name";
-    $email_body = "Esports Committee 2020 Feedback". "Details:\n Name: $name \n". "Email: $email_address\n Feedback: \n $comments";
-    $headers = "From: $email\n";
-    $headers .= "Reply-To: $email_address";
-
-    header('Location: contact-form.html');
+if(empty($_POST["email"])) {
+    $email_error = "Your email must be entered!";
+} else {
+    $user_email = $_POST["email"];
+    echo "Your email address is " .$user_email;
 }
-
-
-
-
-
-
 
 ?>
